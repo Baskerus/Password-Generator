ed3d3a338db85f5059f861b6a4c2d617ac62b4df
Baskerus <baskerus@gmail.com>
2021-11-20 09:23:00 +0100

    javascript functionality

--- js/script.js
+++ js/script.js
@@ -7,7 +7,8 @@ const symbolsToggle = document.querySelector("#symbolsToggle");
 const display = document.querySelector("#display");
 
 lengthDisplay.innerText = rangeSlider.value;
-let characters = "";
+let characters = "abcdefghijklmnopqrstuvwxyz";
+let passwordStrength = 0;
 let hasLowercase = true;
 let hasUppercase = false;
 let hasNumbers = false;
@@ -29,7 +30,6 @@ rangeSlider.addEventListener("input", () => {
 /* ----------- Functions ----------- */
 
 function toggleProperties() {
-  /* Handles slide toggles */
   const currentToggle = event.target.parentNode;
   currentToggle.classList.toggle("active");
   if (event.target.hasAttribute("checked")) {
@@ -55,10 +55,12 @@ function toggleProperties() {
 function generatePassword() {
   let passwordLength = rangeSlider.value;
   const generatedPassword = generateString(passwordLength);
-  display.innerText = generatedPassword;
+  display.innerHTML = `${generatedPassword}<img src="/images/iconmonstr-clipboard-4-12.png" alt="">`;
+  checkPasswordStrength(generatedPassword);
 }
 
 function generateString(length) {
+  /* Generates random string from characters array */
   let result = " ";
 
   const charactersLength = characters.length;
@@ -67,4 +69,54 @@ function generateString(length) {
   }
 
   return result;
+}
+
+function checkPasswordStrength(password) {
+  let passwordStrength = 0;
+
+  /* Adds "points" to password strength based on toggled type of characters */
+  if (lowercaseToggle.classList.contains("active")) {
+    passwordStrength = passwordStrength + 2;
+  }
+  if (uppercaseToggle.classList.contains("active")) {
+    passwordStrength = passwordStrength + 2;
+  }
+  if (numbersToggle.classList.contains("active")) {
+    passwordStrength = passwordStrength + 2;
+  }
+  if (symbolsToggle.classList.contains("active")) {
+    passwordStrength = passwordStrength + 2;
+  }
+
+  /* Multiplies points based on password length */
+  if (password.length < 6) {
+    passwordStrength = passwordStrength * 1;
+  } else if (password.length < 7) {
+    passwordStrength = passwordStrength * 2;
+  } else if (password.length > 7 && password.length < 12) {
+    passwordStrength = passwordStrength * 3;
+  } else if (password.length > 11 && password.length < 16) {
+    passwordStrength = passwordStrength * 4;
+  } else if (password.length > 15 && password.length < 20) {
+    passwordStrength = passwordStrength * 10;
+  } else if (password.length > 19) {
+    passwordStrength = passwordStrength * 12;
+  }
+
+  gradePassword(passwordStrength, password);
+}
+
+function gradePassword(passwordStrength, password) {
+  /* Grades the password based on passwordStrength points */
+  if (passwordStrength < 6 || password.length < 7) {
+    console.log("WEAK");
+  } else if (passwordStrength < 11) {
+    console.log("MEDIUM");
+  } else if (passwordStrength < 25) {
+    console.log("STRONG");
+  } else if (passwordStrength === 96 && password.length == 29) {
+    console.log("UNBREAKABLE");
+  } else if (passwordStrength > 23) {
+    console.log("VERY STRONG");
+  }
 }
